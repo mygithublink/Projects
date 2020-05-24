62b0685e26b6af1c5a076eef95966b85f6e70492
mygithublink <65853367+mygithublink@users.noreply.github.com>
2020-05-24 17:45:26 +0530

    Stock Project Module
    1	Show all data in a read only grid, only SaleQty (col-E) will be editable
    2	User wil be able to enter Sale Quantity
    3	"Entered quantity wiill be validated in the grid cell itself with below parameters:
               1- StockQty : Only available stock could be sold
               2- Entered quantity shoud be in the multiple of SKU"
    4-Total Amount should be calculated automatically.

---
+++ StockProject/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StockProject.Models;
+using System.Collections;
+
+namespace StockProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private StockDataEntities1 db = new StockDataEntities1();
+
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            return View(db.ProductTables.ToList());
+        }
+
+
+        public ActionResult Stock()
+        {
+            return View(db.ProductTables.ToList());
+        }
+
+        [HttpPost]
+
+        public ActionResult Stock(string pList)
+        {
+
+            return null;
+            
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
